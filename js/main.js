8e3e3a0a4aa7f55b2052c09899d9c7b9d9810837
TranDacLam <tdlam2105@gmail.com>
2020-11-09 23:15:05 +0700

    #1

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+	// $('#navbar-main .nav > li').hover(function(){
+	// 	$(this).addClass('open')
+	// 	$(this).find('.has-submenu').addClass('highlighted')
+		
+	// 	$(this).find('.mega-menu').css({'display': 'block', 'top': 'auto', 'left': '0', 'margin-top': '0', 'z-index': '10001'});
+	// 	let offset = $(this).find('.mega-menu').offset() && $(this).find('.mega-menu').offset().left || 0
+	// 	console.log("$(this).find('.mega-menu-content').offset()", $(this).find('.mega-menu-content').offset())
+	// 	$(this).find('.mega-menu-content').css({left: -offset+'px', width: $( window ).width()})
+	// }, function() {
+	// 	$( this ).removeClass( "open" );
+	// 	$(this).find('.has-submenu').removeClass('highlighted');
+	// 	$(this).find('.mega-menu').css({'display': 'none', 'top': 'auto', 'left': '0', 'margin-left': '0', 'margin-top': '0', 'z-index': ''});
+	// })
+
+	// var $header = $("#navbar-main-sticky-wrapper")
+
+	// function onScroll(){
+	// 	$(window).on("scroll", function() {
+	// 		if ($(this).scrollTop() >= 50) {
+	// 			$header.addClass('is-sticky');
+	// 			$('#navbar-main').css({'width': $( window ).width(), 'position': 'fixed', 'top': '0px', 'z-index': 'auto'})
+	// 		}
+	// 		else {      
+	// 			$header.removeClass('is-sticky');
+	// 			$('#navbar-main').attr('style', '')
+	// 		}
+	// 	});
+	// }
+	// onScroll()
+
+	// $('.website-select a').on('click', function(){
+	// 	$('#website-container').toggleClass('website-container-active')
+	// })
+});
