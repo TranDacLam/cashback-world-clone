d34126cb4da07bb6c8baa53ce14e9d91aaf232b1
TranDacLam <tdlam2105@gmail.com>
2020-11-13 00:34:30 +0700

    #2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,68 @@
 $(document).ready(function() {
 
+	var allHeaders = $("#footer #accordion .footer-header");
+	var allPanels = $("#footer #accordion ul");
+	$(window)
+		.on("resize", function() {
+			if (window.matchMedia('(min-width: 751px)').matches) {
+				$("#footer #accordion .footer-header").off("click");
+				allPanels.removeAttr("style");
+				allHeaders.removeClass("open");
+			} else {
+				$("#footer #accordion .footer-header")
+					.off()
+					.on("click", function() {
+						if (!$(this).hasClass("open")) {
+							$(this)
+								.next("ul")
+								.slideDown(200);
+							$(this).addClass("open");
+						} else {
+							$(this).removeClass("open");
+							$(this)
+								.next("ul")
+								.slideUp(200);
+						}
+					});
+			}
+		})
+		.resize();
+
+	$('#btnRegisterNow').on('click', function(e){
+		e.preventDefault();
+		if (!$(this).hasClass("open")) {
+			$('#form-register').slideDown(300);
+			$(this).addClass("open");
+			$([document.documentElement, document.body]).animate({
+				scrollTop: $(this).offset().top - 80
+			}, 1000);
+		} else {
+			$(this).removeClass("open");
+			$('#form-register').slideUp(300);
+		}
+	})
+
+	$('.form-register__password .form-register__password-icon-1').on('click', function(e){
+		$(this).css({'display': 'none'})
+		$(this).next('span').css({'display': 'block'})
+		$('.form-register__password input').attr('type', 'text')
+	})
+	$('.form-register__password .form-register__password-icon-2').on('click', function(e){
+		$(this).css({'display': 'none'})
+		$(this).prev('span').css({'display': 'block'})
+		$('.form-register__password input').attr('type', 'password')
+	})
+	$('.form-register__confirm-password .form-register__confirm-password-icon-1').on('click', function(e){
+		$(this).css({'display': 'none'})
+		$(this).next('span').css({'display': 'block'})
+		$('.form-register__confirm-password input').attr('type', 'text')
+	})
+	$('.form-register__confirm-password .form-register__confirm-password-icon-2').on('click', function(e){
+		$(this).css({'display': 'none'})
+		$(this).prev('span').css({'display': 'block'})
+		$('.form-register__confirm-password input').attr('type', 'password')
+	})
+
 	// $('#navbar-main .nav > li').hover(function(){
 	// 	$(this).addClass('open')
 	// 	$(this).find('.has-submenu').addClass('highlighted')
